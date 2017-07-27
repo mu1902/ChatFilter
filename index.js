055e8ef5a877769f47a85f47bacc4a6b5fb939a5
mu1902 <artemis_1877@163.com>
2017-07-27 16:18:31 +0800

    20170727

--- index.js
+++ index.js
@@ -1,56 +1,73 @@
 var request = require('request');
 var qs = require('querystring');
 var qr = require('qrcode-terminal');
+var $ = require('jquery')(require("jsdom").jsdom().createWindow());
 
-var getUUID = function () {
+var request_promise = function (url, method, formData, qs) {
+    return new Promise(function (resolve, reject) {
+        request({ url: url, method: method, formData: formData, qs: qs }, function (err, httpResponse, body) {
+            if (!err) {
+                resolve(body);
+            } else {
+                reject(null);
+            }
+        });
+    });
+}
+
+var getUUID = async function () {
     var opt = {
         appid: 'wx782c26e4c19acffb',
         fun: 'new',
         lang: 'zh_CN',
         _: +new Date
     };
-    return new Promise(function (resolve, reject) {
-        request.post({ url: "https://login.weixin.qq.com/jslogin", formData: opt }, function (err, httpResponse, body) {
-            if (!err) {
-                var data = qs.parse(body, '; ', ' = ')
-                if (data['window.QRLogin.code'] == '200') {
-                    resolve(data['window.QRLogin.uuid'].slice(1, -2));
-                }
-            }
-            else {
-                console.log(err);
-            }
-        });
-    });
+    var res = await request_promise("https://login.weixin.qq.com/jslogin", "POST", opt, {});
+    if (res) {
+        var data = qs.parse(res, '; ', ' = ')
+        if (data['window.QRLogin.code'] == '200') {
+            return data['window.QRLogin.uuid'].slice(1, -2);
+        }
+    } else {
+        return res;
+    }
+
 };
 
-var checkLogin = function (id) {
+var checkLogin = async function (id) {
     var opt = {
         loginicon: true,
         tip: 0,
         uuid: id,
-        _: +new Date
+        _: +new Date,
+        r: ~new Date
     };
-    return new Promise(function (resolve, reject) {
-        request.get({ url: "https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login", qs: opt }, function (err, httpResponse, body) {
-            if (!err) {
-                var data = qs.parse(body, ';', '=')
-                resolve(data);
-            }
-            else {
-                console.log(err);
-                resolve(null)
-            }
-        });
-    });
+    var res = await request_promise("https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login", "GET", {}, opt);
+    if (res) {
+        var data = qs.parse(res, ';', '=')
+        return data;
+    } else {
+        return res;
+    }
+
 };
 
-async function init() {
+var getTicket = async function (url) {
+    var res = await request_promise(url, "GET", {}, {});
+    if (res) {
+        return res;
+    } else {
+        return res;
+    }
+}
+
+var init = async function () {
     var uuid = await getUUID();
+    //console.log(uuid);
     if (uuid == null) {
         console.log("获取UUID失败");
+        return;
     } else {
-        console.log(uuid);
         console.log("扫码登录");
     }
     qr.generate("https://login.weixin.qq.com/l/" + uuid);
@@ -58,7 +75,7 @@ async function init() {
         var loginState = await checkLogin(uuid);
         if (loginState["window.code"] == '200') {
             console.log("确认登录");
-            console.log(loginState["\nwindow.redirect_uri"]);
+            var redirect_uri = loginState["\nwindow.redirect_uri"].slice(1, -1) + '&fun=new';
             break;
         }
         if (loginState["window.code"] == '201') {
@@ -71,6 +88,9 @@ async function init() {
             return;
         }
     }
+    var ticket = await getTicket(redirect_uri);
+    var $ticket = $(ticket);
+    console.log($ticket.find('skey').text());
 };
 
 init();
